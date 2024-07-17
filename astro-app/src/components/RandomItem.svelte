21a9feb13b84649ea233e9f37430bd35a3b0bfaa
Hazel O'Driscoll <hazel_driscoll@hotmail.com>
2024-07-17 21:04:01 +0100

    Update index.astro and add button for random item

---
+++ astro-app/src/components/RandomItem.svelte
@@ -0,0 +1,20 @@
+<!-- src/components/RandomItem.svelte -->
+<script>
+    let items = ['Steak Fahitjas', 'Beef Tacos', 'Roast Chicken', 'Chicken Curry'];
+    let randomItem = '';
+  
+    function getRandomItem() {
+      console.log('Button clicked');
+      const randomIndex = Math.floor(Math.random() * items.length);
+      randomItem = items[randomIndex];
+      console.log('Random item:', randomItem);
+    }
+  </script>
+  
+  <button class="btn btn-primary btn-lg" on:click={getRandomItem}>Get Suggestions</button>
+  <br>
+  <br>
+  {#if randomItem}
+    <p><b>Option:</b> {randomItem}</p>
+  {/if}
+  
