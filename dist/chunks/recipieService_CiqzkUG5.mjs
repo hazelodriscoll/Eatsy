eb1eeae19ffec939f2c12ec45141f74993e7930f
Hazel O'Driscoll <hazel_driscoll@hotmail.com>
2024-08-04 22:14:20 +0100

    feature/expand-favourites: Add functional favourites feature to cards + update styles

---
+++ dist/chunks/recipieService_CiqzkUG5.mjs
@@ -0,0 +1,55 @@
+function constructQueryString(params) {
+  return new URLSearchParams(params).toString();
+}
+
+// Get Random Recipies
+async function getRecipies(params) {
+  const baseURL = "https://api.spoonacular.com/recipes/random";
+  const queryString = constructQueryString(params); // Convert params object to query string
+  const fullURL = `${baseURL}?${queryString}`;
+  console.log("fullURL:" + fullURL);
+  try {
+    const response = await fetch(fullURL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching recipes:", error);
+    return null;
+  }
+}
+
+// Get Recipes by Search Query and Include Instructions
+async function searchRecipies(params) {
+  const baseURL = "https://api.spoonacular.com/recipes/complexSearch";
+  const queryString = constructQueryString(params); // Convert params object to query string
+  const fullURL = `${baseURL}?${queryString}`;
+  console.log("Full URL:", fullURL);
+
+  try {
+    const response = await fetch(fullURL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log("Fetched search data:", data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching recipes:", error);
+    return null;
+  }
+}
+
+export { getRecipies as g, searchRecipies as s };
