ddc70834a9d0084e5131094c8dacf13e0ed048cb
Hazel O'Driscoll <hazel_driscoll@hotmail.com>
2024-08-06 20:26:30 +0100

    feature/change-search-to-sessionStorage: use session storage instead for search

---
+++ dist/_astro/BaseLayout.astro_astro_type_script_index_1_lang.Dy2g0fqL.js
@@ -0,0 +1,1 @@
+document.getElementById("searchForm").addEventListener("submit",function(t){t.preventDefault();const e=document.getElementById("searchQuery").value;e&&(sessionStorage.setItem("searchQuery",e),window.location.href="/Eatsy/searchResults")});
